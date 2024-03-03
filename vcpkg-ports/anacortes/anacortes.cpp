d4646195194140e730f8a57d9e260d4196ac19a0
pavel komlev <pkomlev@example.com>
2024-03-02 20:58:26 -0800

    add transitive deps

--- vcpkg-ports/anacortes/anacortes.cpp
+++ vcpkg-ports/anacortes/anacortes.cpp
@@ -1,7 +1,9 @@
-#pragma once
-
 #include "anacortes/anacortes.h"
 
+#include <fmt/format.h>
+
 namespace anacortes {
-std::string hello_username(const std::string& user) { return user; }
+std::string hello_username(const std::string& user) {
+  return fmt::format("Welcome to the coastal city of Anacortes, {}! ", user);
+}
 }  // namespace anacortes
